c385ae8070d6160bf7dc3acb8dd4189df61a083f
gooddltmdqls <1415wwwh@gmail.com>
2022-06-16 07:49:31 +0900

    asdfchat

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,13 @@ import { WebSocketServer } from 'ws';
 import server from './server';
 
 const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+    console.log("Recieved a connection.");
+    ws.send("Waiting for authentication...");
+
+    ws.on("message", (msg) => {
+        console.log("Recieved a message: " + msg);
+        ws.send(msg);
+    });
+});
